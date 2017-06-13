2ad6d96d2de644b4d9ad72b75ad0d91727c1119d
ariellewriter <missarielle@gmail.com>
2017-06-13 18:54:15 -0400

    initial comit

---
+++ MythrilCollective/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MythrilCollective
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
